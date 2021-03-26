8fad09f9ef1bc711d556bfa4532527bacb7bd8b1
Fabio Navarrete <fabioanh@gmail.com>
2021-03-26 23:56:55 +0100

    Initial commit: Spring boot project structure

---
+++ src/test/java/com/ubitricity/carparkubi/CarparkUbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubitricity.carparkubi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkUbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
