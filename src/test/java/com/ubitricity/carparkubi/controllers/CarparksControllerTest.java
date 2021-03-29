23669c8463a0213d17f68eabf13d769645c039b8
Fabio Navarrete <fabioanh@gmail.com>
2021-03-29 13:45:35 +0200

    Documentation added

--- src/test/java/com/ubitricity/carparkubi/controllers/CarparksControllerTest.java
+++ src/test/java/com/ubitricity/carparkubi/controllers/CarparksControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -23,7 +24,12 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -51,7 +57,6 @@ class CarparksControllerTest {
         // given
         var postBody = """
                 {
-                    "id": "CP3",
                     "connected": true
                 }
                 """;
@@ -65,13 +70,29 @@ class CarparksControllerTest {
         when(carparkUbi.connect("CP3"))
                 .thenReturn(new ChargingPoint("CP3", 20, true));
         // when
-        ResultActions response = this.mockMvc.perform(put("/carparks/ubi/chargingPoints/CP3")
+        ResultActions response = this.mockMvc.perform(RestDocumentationRequestBuilders.put("/carparks/{carparkName}/chargingPoints/{chargingPointId}", "ubi", "CP3")
                 .content(postBody)
                 .contentType("application/json")
                 .characterEncoding("utf-8"));
         // then
         response.andExpect(status().isOk())
-                .andExpect(content().json(expectedJson));
+                .andExpect(content().json(expectedJson))
+                .andDo(document("connect-charging-point",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("carparkName").description("Identifier of the carpark. For now only the value **'ubi'** is allowed"),
+                                parameterWithName("chargingPointId").description("Identifier of the charging point to be connected/disconnected.")
+                        ),
+                        requestFields(
+                                fieldWithPath("connected").type("Boolean").description("The `true` value indicates the charging point should be connected")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type("String").description("Identifier of the affected charging point"),
+                                fieldWithPath("current").type("Number").description("Current assigned to the charging point"),
+                                fieldWithPath("connected").type("Boolean").description("Value should be `true` as a connect operation was done")
+                        )
+                ));
     }
 
     @Test
@@ -79,7 +100,6 @@ class CarparksControllerTest {
         // given
         var postBody = """
                 {
-                    "id": "CP42",
                     "connected": true
                 }
                 """;
@@ -91,7 +111,12 @@ class CarparksControllerTest {
                 .contentType("application/json")
                 .characterEncoding("utf-8"));
         // then
-        response.andExpect(status().isNotFound());
+        response.andExpect(status().isNotFound())
+                .andDo(document("charging-point-not-found",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())
+                        )
+                );
     }
 
     @Test
@@ -99,7 +124,6 @@ class CarparksControllerTest {
         // given
         var postBody = """
                 {
-                    "id": "CP3",
                     "connected": false
                 }
                 """;
@@ -113,13 +137,29 @@ class CarparksControllerTest {
         when(carparkUbi.disconnect("CP3"))
                 .thenReturn(new ChargingPoint("CP3", 0, false));
         // when
-        ResultActions response = this.mockMvc.perform(put("/carparks/ubi/chargingPoints/CP3")
+        ResultActions response = this.mockMvc.perform(RestDocumentationRequestBuilders.put("/carparks/{carparkName}/chargingPoints/{chargingPointId}", "ubi", "CP3")
                 .content(postBody)
                 .contentType("application/json")
                 .characterEncoding("utf-8"));
         // then
         response.andExpect(status().isOk())
-                .andExpect(content().json(expectedJson));
+                .andExpect(content().json(expectedJson))
+                .andDo(document("disconnect-charging-point",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("carparkName").description("Identifier of the carpark. For now only the value **'ubi'** is allowed"),
+                                parameterWithName("chargingPointId").description("Identifier of the charging point to be connected/disconnected.")
+                        ),
+                        requestFields(
+                                fieldWithPath("connected").type("Boolean").description("The `false` value indicates the charging point should be disconnected")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type("String").description("Identifier of the affected charging point"),
+                                fieldWithPath("current").type("Number").description("Current assigned to the charging point should be `0` as the charging point was disconnected"),
+                                fieldWithPath("connected").type("Boolean").description("Value should be `false` as it was disconnected")
+                        ))
+                );
     }
 
     @Test
@@ -127,7 +167,6 @@ class CarparksControllerTest {
         // given
         var postBody = """
                 {
-                    "id": "CP42",
                     "connected": false
                 }
                 """;
@@ -148,17 +187,21 @@ class CarparksControllerTest {
         // given
         var postBody = """
                 {
-                    "id": "CP3",
                     "connected": true
                 }
                 """;
         // when
-        ResultActions response = this.mockMvc.perform(put("/carparks/non_existent_carpark/chargingPoints/CP3")
+        ResultActions response = this.mockMvc.perform(RestDocumentationRequestBuilders.put("/carparks/{carparkName}/chargingPoints/{chargingPointId}", "non_existent_carpark", "CP3")
                 .content(postBody)
                 .contentType("application/json")
                 .characterEncoding("utf-8"));
         // then
-        response.andExpect(status().isNotFound());
+        response.andExpect(status().isNotFound())
+                .andDo(document("carpark-not-found",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())
+                        )
+                );
     }
 
     @Test
@@ -226,12 +269,24 @@ class CarparksControllerTest {
                 ]
                 """;
         // when
-        ResultActions response = this.mockMvc.perform(get("/carparks/ubi/chargingPoints")
+        ResultActions response = this.mockMvc.perform(RestDocumentationRequestBuilders.get("/carparks/{carparkName}/chargingPoints", "ubi")
                 .contentType("application/json")
                 .characterEncoding("utf-8"));
         // then
         response.andExpect(status().isOk())
-                .andExpect(content().json(expectedJson));
+                .andExpect(content().json(expectedJson))
+                .andDo(document("get-charging-points",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("carparkName").description("Identifier of the carpark. For now only the value **'ubi'** is allowed")
+                        ),
+                        responseFields(
+                                fieldWithPath("[].id").type("String").description("Identifier of the charging point"),
+                                fieldWithPath("[].current").type("Number").description("Current assigned to the charging point"),
+                                fieldWithPath("[].connected").type("Boolean").description("Indicates whether the charging point is connected or disconnected")
+                        ))
+                );
     }
 
     @Test
